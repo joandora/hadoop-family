4bc887589ae639ac72edf1da5e4e34f6b6ca7d8f
teddy <858305351@qq.com>
2016-07-01 09:51:00 +0800

    增加注释-并测试代码

--- mahout-0.8/src/main/java/org/conan/mymahout/recommendation/book/BookEvaluator.java
+++ mahout-0.8/src/main/java/org/conan/mymahout/recommendation/book/BookEvaluator.java
@@ -8,21 +8,23 @@ import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
-
+/**选出“评估推荐器”验证得分较高的算法<br>
+ * itemEuclideanNoPref评估的结果最好<br>
+ **/
 public class BookEvaluator {
 
     final static int NEIGHBORHOOD_NUM = 2;
     final static int RECOMMENDER_NUM = 3;
 
     public static void main(String[] args) throws TasteException, IOException {
-        String file = "datafile/book/rating.csv";
-        DataModel dataModel = RecommendFactory.buildDataModel(file);
+        String localFile = BookFilterGenderResult.class.getResource("/datafile/book/rating.csv").getPath();
+        DataModel dataModel = RecommendFactory.buildDataModel(localFile);
         userEuclidean(dataModel);
         userLoglikelihood(dataModel);
         userEuclideanNoPref(dataModel);
         itemEuclidean(dataModel);
         itemLoglikelihood(dataModel);
-        itemEuclideanNoPref(dataModel);
+        itemEuclideanNoPref(dataModel); // 根据结果 评估的结果最好
         slopeOne(dataModel);
     }
 
@@ -97,5 +99,29 @@ public class BookEvaluator {
         RecommendFactory.statsEvaluator(recommenderBuilder, null, dataModel, 2);
         return recommenderBuilder;
     }
+    /*
+    *
+userEuclidean
+AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:0.08333331346511841
+Recommender IR Evaluator: [Precision:0.3010752688172043,Recall:0.08542713567839195]
+userLoglikelihood
+AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:1.8290335337320962
+Recommender IR Evaluator: [Precision:0.11764705882352945,Recall:0.017587939698492466]
+userEuclideanNoPref
+AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:4.438433652832394
+Recommender IR Evaluator: [Precision:0.09045226130653267,Recall:0.09296482412060306]
+itemEuclidean
+AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:1.4149588317756185
+Recommender IR Evaluator: [Precision:0.0,Recall:0.0]
+itemLoglikelihood
+AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:2.402807165823069
+Recommender IR Evaluator: [Precision:0.0,Recall:0.0]
+itemEuclideanNoPref
+AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:2.5113286750771153
+Recommender IR Evaluator: [Precision:0.6005025125628134,Recall:0.6055276381909548]
+slopeOne
+AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:2.7985169042221254
+Recommender IR Evaluator: [Precision:0.0,Recall:0.0]
+*/
 
 }
--- mahout-0.8/src/main/java/org/conan/mymahout/recommendation/book/BookFilterGenderResult.java
+++ mahout-0.8/src/main/java/org/conan/mymahout/recommendation/book/BookFilterGenderResult.java
@@ -14,15 +14,22 @@ import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.recommender.IDRescorer;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-
+import org.conan.mymahout.hdfs.HdfsDAO;
+/**
+ * 与BookResult类似，不过增加了过滤器，只保留男性用户的图书列表<br>
+ * 由于只保留男性的评分记录，数据量就变得比较少了，基于用户的协同过滤算法，已经没有输出的结果了。基于物品的协同过滤算法，结果集也有所变化<br>
+ * itemEuclideanNoPref给用户65最先推荐887的图书<br>
+ * 再查看rating.csv发现，有4个用户都给887打过分，单只有两名男性：184和186<br>
+ * 通过分析发现：用户65与用户186都给图书65和图书375打过分、用户186，还给图书887打过分，所以对于给65用户推荐图书887，是合理的<br>
+ */
 public class BookFilterGenderResult {
 
     final static int NEIGHBORHOOD_NUM = 2;
     final static int RECOMMENDER_NUM = 3;
 
     public static void main(String[] args) throws TasteException, IOException {
-        String file = "datafile/book/rating.csv";
-        DataModel dataModel = RecommendFactory.buildDataModel(file);
+        String localFile = BookFilterGenderResult.class.getResource("/datafile/book/rating.csv").getPath();
+        DataModel dataModel = RecommendFactory.buildDataModel(localFile);
         RecommenderBuilder rb1 = BookEvaluator.userEuclidean(dataModel);
         RecommenderBuilder rb2 = BookEvaluator.itemEuclidean(dataModel);
         RecommenderBuilder rb3 = BookEvaluator.userEuclideanNoPref(dataModel);
@@ -43,7 +50,8 @@ public class BookFilterGenderResult {
      * 对用户性别进行过滤
      */
     public static void filterGender(long uid, RecommenderBuilder recommenderBuilder, DataModel dataModel) throws TasteException, IOException {
-        Set<Long> userids = getMale("datafile/book/user.csv");
+        String localFile = BookFilterGenderResult.class.getResource("/datafile/book/user.csv").getPath();
+        Set<Long> userids = getMale(localFile);
 
         //计算男性用户打分过的图书
         Set<Long> bookids = new HashSet<Long>();
@@ -98,4 +106,23 @@ class FilterRescorer implements IDRescorer {
     public boolean isFiltered(long id) {
         return userids.contains(id);
     }
+
+    /**
+     *
+     userEuclidean
+     AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:0.0
+     Recommender IR Evaluator: [Precision:0.3010752688172043,Recall:0.08542713567839195]
+     itemEuclidean
+     AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:1.374159288367415
+     Recommender IR Evaluator: [Precision:0.0,Recall:0.0]
+     userEuclideanNoPref
+     AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:5.0110146223780605
+     Recommender IR Evaluator: [Precision:0.09045226130653267,Recall:0.09296482412060306]
+     itemEuclideanNoPref
+     AVERAGE_ABSOLUTE_DIFFERENCE Evaluater Score:2.543966384663835
+     Recommender IR Evaluator: [Precision:0.6005025125628134,Recall:0.6055276381909548]
+     userEuclidean       =>uid:65,
+     itemEuclidean       =>uid:65,(784,8.090909)(276,8.000000)(476,7.666667)
+     userEuclideanNoPref =>uid:65,
+     itemEuclideanNoPref =>uid:65,(887,2.250000)(356,2.166667)(430,1.866667)**/
 }
